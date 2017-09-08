09b2ab4fd8caeaa16e510380598450afb6685c56
libccy <libccy@gmail.com>
2017-09-08 13:10:53 +0800

    fix adjoint video

--- code/run_wavefield_propagation.m
+++ code/run_wavefield_propagation.m
@@ -24,7 +24,7 @@ if exist('prevmsg') reverseStr = repmat(sprintf('\b'), 1, length(prevmsg));
 else reverseStr = '';
 end
 prevmsg = sprintf('iterating...');
-fprintf([reverseStr, prevmsg]); 
+fprintf([reverseStr, prevmsg]);
 % disp 'iterating...'
 
 position_figures;
@@ -39,7 +39,7 @@ if exist('prevmsg') reverseStr = repmat(sprintf('\b'), 1, length(prevmsg));
 else reverseStr = '';
 end
 prevmsg = sprintf('iterating... Percentage completed :       ');
-fprintf(1, [reverseStr, prevmsg]); 
+fprintf(1, [reverseStr, prevmsg]);
 
 %%
 for n=1:nt
@@ -48,7 +48,7 @@ for n=1:nt
         blips = sprintf('\b\b\b\b\b\b%6.0f',n/nt*100);
         fprintf(1,blips);
     end
-    
+
             % store time-reversed history ----------------------------------------
         sfe = store_fw_every;
         if (mod(n,sfe)==0)
@@ -63,10 +63,10 @@ for n=1:nt
                 uz_forward(nt/sfe+1-n/sfe,:,:)=uz(:,:);
             end
         end
-    
+
     %- compute divergence of current stress tensor ------------------------
-    %  forward -- unit of force: [kg m^-2 s^-2] = [N] 
-    
+    %  forward -- unit of force: [kg m^-2 s^-2] = [N]
+
     if(strcmp(wave_propagation_type,'SH'))
         DSY=div_s(sxy,szy,dx,dz,nx,nz,order);
     elseif(strcmp(wave_propagation_type,'PSV'))
@@ -75,16 +75,16 @@ for n=1:nt
         DSY=div_s(sxy,szy,dx,dz,nx,nz,order);
         [DSX,DSZ]=div_s_PSV(sxx,szz,sxz,dx,dz,nx,nz,order);
     end
-    
+
 %       test.DSX_before_stf1(n) = DSX(src_x_id(1),src_z_id(1));
 %       test.DSX_before_stf2(n) = DSX(src_x_id(2),src_z_id(2));
-    
+
     %- add point sources --------------------------------------------------
-    
+
     if (strcmp(simulation_mode,'forward') || strcmp(simulation_mode,'forward_green') ||  strcmp(simulation_mode,'adjoint') )
-    
+
         for is=1:ns
-            
+
             if(strcmp(wave_propagation_type,'SH'))
                 DSY(src_x_id(is),src_z_id(is))= DSY(src_x_id(is),src_z_id(is)) + stf{is}.y(n);
             elseif(strcmp(wave_propagation_type,'PSV'))
@@ -97,19 +97,19 @@ for n=1:nt
             end
 
         end
-        
+
 
     end
-    
-    
+
+
     %- update velocity field ----------------------------------------------
-    
+
 %     test.vx_source1 = vx(src_x_id(1),src_z_id(1)) + ...
 %             dt*DSX(src_x_id(1),src_z_id(1)) / rho(src_x_id(1),src_z_id(1));
 %     test.vx_source2 = vx(src_x_id(2),src_z_id(2)) + ...
 %             dt*DSX(src_x_id(2),src_z_id(2)) / rho(src_x_id(2),src_z_id(2));
 % size(test_vx_source)
-    
+
     if(strcmp(wave_propagation_type,'SH'))
         vy=vy+dt*DSY./rho;
     elseif(strcmp(wave_propagation_type,'PSV'))
@@ -120,10 +120,10 @@ for n=1:nt
         vx=vx+dt*DSX./rho;
         vz=vz+dt*DSZ./rho;
     end
-    
+
                         % just a test to see whether DS and v produce values
                         % [max(max(DS)),max(max(DSX)),max(max(DSZ)); max(max(v)),max(max(vx)),max(max(vz))]
-    
+
 %     test.stf(n) = stf(1,1,n);
 %     test.DSX1(n) = DSX(src_x_id(1),src_z_id(1));
 %     test.DSX2(n) = DSX(src_x_id(2),src_z_id(2));
@@ -132,9 +132,9 @@ for n=1:nt
 %               '  DSX  ', num2str(DSX(src_x_id(1),src_z_id(1))) ...
 %               '  vx   ', num2str(vx(src_x_id(1),src_z_id(1)))]) % ...
 %               '  testvx ', num2str(test_vx_source)])
-    
+
     %- apply absorbing boundary taper -------------------------------------
-    
+
     if(strcmp(wave_propagation_type,'SH'))
         vy=vy.*absbound;
     elseif(strcmp(wave_propagation_type,'PSV'))
@@ -145,9 +145,9 @@ for n=1:nt
         vx=vx.*absbound;
         vz=vz.*absbound;
     end
-    
+
     %- compute derivatives of current velocity and update stress tensor ---
-    
+
     if(strcmp(wave_propagation_type,'SH'))
         sxy=sxy+ dt* mu.* dx_v(vy,dx,dz,nx,nz,order);
         szy=szy+ dt* mu.* dz_v(vy,dx,dz,nx,nz,order);
@@ -168,9 +168,9 @@ for n=1:nt
         szz=szz+dt*( (lambda+2*mu).*dvzdz(:,:) + lambda.*dvxdx(:,:) );
         sxz=sxz+dt*( mu.* (dvxdz(:,:) + dvzdx(:,:)) );
     end
-    
+
     %- compute displacement field -----------------------------------------
-    
+
     if(strcmp(wave_propagation_type,'SH'))
         uy = uy + vy.*dt;
     elseif(strcmp(wave_propagation_type,'PSV'))
@@ -181,14 +181,14 @@ for n=1:nt
         uy = uy + vy.*dt;
         uz = uz + vz.*dt;
     end
-    
-    
+
+
     %% forward calculations
     if (strcmp(simulation_mode,'forward'))
-        
-        
+
+
         % record velocity seismograms -------------------------------------
-        
+
         for k=1:n_receivers
             if(strcmp(wave_propagation_type,'SH'))
                 v_rec{k}.y(n)=vy(rec_x_id(k),rec_z_id(k));
@@ -201,12 +201,12 @@ for n=1:nt
                 v_rec{k}.z(n)=vz(rec_x_id(k),rec_z_id(k));
             end
         end
-        
-        
+
+
         % store time-reversed history ----------------------------------------
 %         store_fw_every = 10; % These params also defined below at calc knl
         sfe = store_fw_every;
-        
+
         % save every 5th velocity&displacement field to the big-ass 3 direction matrix
 
         if (mod(n,sfe)==0)
@@ -231,44 +231,46 @@ for n=1:nt
             end
         end
 
-    
+
         % plot velocity field every so manyth time step -------------------
-        
+
         if (mod(n,plot_every)==0)
 %             disp([num2str(n), ' ', num2str(n*dt), ' ', num2str(floor(n*dt),'%4.0f')]);
 %             disp(['time: ', num2str(n*dt)]);
             plot_velocity_field;
         end
-        
-        
-        
-    %% adjoint calculations    
+
+
+
+    %% adjoint calculations
     elseif(strcmp(simulation_mode,'adjoint'))
-        
+
         % plot and compute kernel every sfe-th iteration -----------------------
 %         store_fw_every = 10; % These params also defined above at save fwd
         sfe = store_fw_every;
 %         if (mod(n,sfe)==0)
         if (mod(n + (sfe-1),sfe)==0)
-                        
+
             % calculate kernels
             compute_kernels;
 %             [K, K_parts] = compute_kernels(vx, vy, vz, vx_forward);
-            
-            % plot adjoint fields (i.e. adjoint field + the above)
-            if (mod(n,plot_every)==0)
-                plot_adjoint_field;
-            end
-            
+
         end
+
+
+        % plot adjoint fields (i.e. adjoint field + the above)
+        if (mod(n,plot_every)==0)
+            plot_adjoint_field;
+        end
+        
     end
-    
+
 end
 blaps = [prevmsg, blips];
 reverseStr = repmat(sprintf('\b'), 1, length(blaps));
 if exist('prevmsg') reverseStr = repmat(sprintf('\b'), 1, length(prevmsg));
 else reverseStr = '';
 end
-fprintf(1, reverseStr); 
+fprintf(1, reverseStr);
 
 % disp ' ';
