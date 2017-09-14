ba775d683da12195360c70999c4134620e2c4c62
libccy <libccy@gmail.com>
2017-09-14 14:50:53 +0800

    test2

--- code/run_adjoint_test.m
+++ code/run_adjoint_test.m
@@ -1,4 +1,11 @@
 [v_rec,t,u_fw,v_fw,rec_x,rec_z]=run_forward;
-stf = prepare_stf;
-stf = {stf.stf};
-K = run_adjoint(u_fw,v_fw,stf);
+% stf = prepare_stf;
+% stf = {stf.stf};
+% K = run_adjoint(u_fw,v_fw,stf);
+
+subplot(2,1,1)
+plot(0.1:0.1:500,v_rec{1}.x);
+xlabel('vx')
+subplot(2,1,2)
+plot(0.1:0.1:500,v_rec{1}.z);
+xlabel('vz')
